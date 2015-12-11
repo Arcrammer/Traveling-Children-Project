0b6f4069425b7b2449e83ccf10beb4b5f5c8d9aa
Christina Thorpe <cdtdesign@fullsail.edu>
2015-12-10 19:36:06 -0500

    Removed JS tooltip script

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -33,11 +33,6 @@
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-	<script>
-	$(function () {
-		$('[data-toggle="tooltip"]').tooltip();
-	});
-	</script>
     @yield('extra_scripts')
   </body>
 </html>
